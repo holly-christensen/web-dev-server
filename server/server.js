8838ec55af3ea29d48c27e941af636912d6778a3
Holly Christensen <holly.christensen@comcast.net>
2022-04-12 18:51:25 -0400

    remove unused import

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import helloController from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitController from "./controllers/tuits-controller.js";
-// const express = require('express')
 const app = express()
 
 app.use(cors());
