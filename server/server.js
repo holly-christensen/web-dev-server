0378a034073253e7c503985806312763480665f0
Holly Christensen <holly.christensen@comcast.net>
2022-04-12 19:03:19 -0400

    update express import path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from "express";
 const app = express();
 import cors from "cors";
 import helloController from "./controllers/hello-controller.js";
